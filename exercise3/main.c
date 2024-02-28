7a67e1de1af386afdb8e363ca74d3f807545eaba
el-shago <sperezmaris@gmail.com>
2024-02-28 13:43:52 -0800

    Ejercicio 3 UwU

--- exercise3/main.c
+++ exercise3/main.c
@@ -5,12 +5,48 @@
 
 #define NumberOfThreads 10
 #define size 100
+#define b size/NumberOfThreads
 
+int stotal;
+
+pthread_t T[NumberOfThreads];
 pthread_mutex_t the_mutex;
-pthread_mutex_init(&the_mutex, 0);
+
 
 int array[size];
 
+void * suma(void*i){
+    int tid = i;
+    int inicio = tid * b;
+    int fin = (tid + 1) * b;
+    int subsuma;
+
+    for(int j=inicio; j<fin; j++){
+        subsuma += array[j];
+        pthread_mutex_lock(&the_mutex);
+        stotal += subsuma;
+        pthread_mutex_unlock(&the_mutex);
+    }
+}
+
+int main (void){
+    pthread_mutex_init(&the_mutex, 0);
+
+    for(int i = 0; i < NumberOfThreads; i++){
+        pthread_create(&T[i], NULL, suma, (void *)i);
+    }
+
+    for(int i = 0; i < NumberOfThreads; i++){
+        pthread_join(T[i], NULL);
+    }
+
+    printf("Total sum: %d\n", stotal);
+
+    pthread_mutex_destroy(&the_mutex);
+
+    return 0;
+    
+}
 
 
 
