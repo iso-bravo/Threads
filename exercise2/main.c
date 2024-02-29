4673d75b0d14eb036b69e34b76533ca498421005
el-shago <sperezmaris@gmail.com>
2024-02-28 20:44:05 -0800

    Cambios

--- exercise2/main.c
+++ exercise2/main.c
@@ -6,7 +6,7 @@
 #define NumberOfThreads 10
 #define NumberOfRequest 100
 
-char FILENAME[]="Contador.txt";
+char FILENAME[] = "Contador.txt";
 FILE *file = NULL;
 
 pthread_mutex_t the_mutex;
@@ -15,60 +15,66 @@ int read_file();
 int write_file(int);
 void *contador(void *);
 
-int main(void){
-	//Configuration
-	if((file=fopen(FILENAME,"w"))==NULL){
-		printf("The file could't be created :(");
-		return 0;
-	}
-	fprintf(file, "%d", 0);
-	fclose(file);
-	//Semaphore creation
-	pthread_mutex_init(&the_mutex, 0); 
-	pthread_t T[NumberOfThreads];
-	//Thread creation
-	int i;
-	for(i=0; i<NumberOfThreads;i++){
-		pthread_create(&T[i],0,contador,0); 
-		pthread_join(&T[i],0);
-	}
-	for(i=0; i<NumberOfThreads;i++){
-		pthread_join(&T[i],0);
-	}
-	pthread_mutex_destroy(&the_mutex);
-	wait(NULL);
+int main(void) {
+    if ((file = fopen(FILENAME, "w")) == NULL) {
+        printf("The file couldn't be created :(\n");
+        return 0;
+    }
+    fprintf(file, "%d", 0);
+    fclose(file);
+
+    pthread_mutex_init(&the_mutex, NULL);
+    pthread_t T[NumberOfThreads];
+
+    int i;
+    for (i = 0; i < NumberOfThreads; i++) {
+        int *thread_id = malloc(sizeof(int));
+        *thread_id = i;
+        pthread_create(&T[i], NULL, contador, thread_id);
+    }
+
+    for (i = 0; i < NumberOfThreads; i++) {
+        pthread_join(T[i], NULL);
+    }
+
+    pthread_mutex_destroy(&the_mutex);
+
+    wait(NULL);
+
+    return 0;
 }
 
-void *contador(void *id){
-	//Semaphore blocking
-	for(int i=0;i<NumberOfRequest;i++){
-		pthread_mutex_lock(&the_mutex);
-		read_file(file);
-		i++;
-		write_file(file);
-		pthread_mutex_unlock(&the_mutex);
-	}
-	pthread_exit(NULL);
+void *contador(void *id) {
+    int thread_id = *((int *)id);
+    free(id);
+
+    for (int i = 0; i < NumberOfRequest; i++) {
+        pthread_mutex_lock(&the_mutex);
+        int a = read_file();
+        a++;
+        write_file(a);
+        pthread_mutex_unlock(&the_mutex);
+    }
+    pthread_exit(NULL);
 }
 
-
-int read_file(){
-	int contador;
-	if((file=fopen(FILENAME,"r"))==NULL){
-		printf("The file could't be readed :(");
-		return -1;
-	}
-	fscanf(file,"%d",&contador);
-	fclose(file);
-	return contador;
+int read_file() {
+    int contador;
+    if ((file = fopen(FILENAME, "r")) == NULL) {
+        printf("The file couldn't be read :(\n");
+        return -1;
+    }
+    fscanf(file, "%d", &contador);
+    fclose(file);
+    return contador;
 }
 
-int write_file(int c){
-	if((file=fopen(FILENAME,"w"))==NULL){
-		printf("The file could't be created :(");
-		return -1;
-	}
-	fprintf(file,"%d",c);
-	fclose(file);
-	return 1;
+int write_file(int c) {
+    if ((file = fopen(FILENAME, "w")) == NULL) {
+        printf("The file couldn't be created :(\n");
+        return -1;
+    }
+    fprintf(file, "%d", c);
+    fclose(file);
+    return 1;
 }
--- exercise3/main.c
+++ exercise3/main.c
@@ -8,29 +8,34 @@
 #define b size/NumberOfThreads
 
 int stotal;
-
 pthread_t T[NumberOfThreads];
 pthread_mutex_t the_mutex;
 
-
 int array[size];
 
-void * suma(void*i){
-    int tid = i;
+void *suma(void *i){
+    int tid = (int)i;
     int inicio = tid * b;
     int fin = (tid + 1) * b;
-    int subsuma;
+    int subsuma = 0;
 
     for(int j=inicio; j<fin; j++){
         subsuma += array[j];
-        pthread_mutex_lock(&the_mutex);
-        stotal += subsuma;
-        pthread_mutex_unlock(&the_mutex);
     }
+
+    pthread_mutex_lock(&the_mutex);
+    stotal += subsuma;
+    pthread_mutex_unlock(&the_mutex);
+
+    return NULL;
 }
 
 int main (void){
-    pthread_mutex_init(&the_mutex, 0);
+    pthread_mutex_init(&the_mutex, NULL);
+
+    for(int i = 0; i < size; i++){
+        array[i] = i + 1;
+    }
 
     for(int i = 0; i < NumberOfThreads; i++){
         pthread_create(&T[i], NULL, suma, (void *)i);
@@ -45,8 +50,4 @@ int main (void){
     pthread_mutex_destroy(&the_mutex);
 
     return 0;
-    
 }
-
-
-
